d89938d3bcbb06d072e772ea1026fff2b0c89cb5
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-11-21 17:49:38 +0530

    initial commit with tailwind initialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-red-900 text-2xl">hello world</p>
+    </div>
+  );
+}
+
+export default App;
