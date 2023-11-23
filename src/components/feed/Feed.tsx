165c0262d66f6410bd0895b68687b843833ded26
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-11-23 15:39:31 +0530

    search onclick bug fix

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -82,7 +82,7 @@ function Feed(props: any) {
             placeholder="Software Engineer, full stack developer, React, Node"
           />
           <button
-            onClick={handleSearch}
+            onClick={() => listJobs(userInput)}
             type="submit"
             className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
           >
