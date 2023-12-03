748d8e51a65e07ef071f6a3d00c61e08f618c9ed
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-12-03 14:56:17 +0530

    fix search button placement on mobile/large screen

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -76,11 +76,18 @@ function Feed() {
           <button
             onClick={handleSearch}
             type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+            className="hidden text-white md:block absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
           >
             Search
           </button>
         </div>
+        <button
+          onClick={handleSearch}
+          type="submit"
+          className="md:hidden text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+        >
+          Search
+        </button>
       </div>
       {isLoading ? (
         <div className="flex justify-center items-center">Loading...</div>
